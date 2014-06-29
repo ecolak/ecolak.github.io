f2e49201a9ae3ecaa191881c2bb27c95297277e3
Emre Colak <colemre@gmail.com>
2014-06-29 15:06:57 -0700

    fb login change

--- frips/js/main.js
+++ frips/js/main.js
@@ -16,17 +16,11 @@ $(document).ready(function() {
     	FB.getLoginStatus(function(response) {
         	if (response.status === 'connected') {
     	      // Logged into your app and Facebook.
-    	      testAPI();
-    	      setLocations();
-    	    } else if (response.status === 'not_authorized') {
-    	      // The person is logged into Facebook, but not your app.
-    	      console.log('Please log into this app.');
+        		testAPI();
+        		setLocations();
     	    } else {
-    	      // The person is not logged into Facebook, so we're not sure if
-    	      // they are logged into this app or not.
-    	      console.log('Please log into Facebook.');
-    	      $(location).attr('href','login.html');
-    	      return false;
+    	    	$(location).attr('href','login.html');
+    	    	return false;
     	    }
         });
 
