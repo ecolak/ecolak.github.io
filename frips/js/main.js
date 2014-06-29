548e4ad50366f2af9250be07078d2bc9c8e8eff9
Emre Colak <colemre@gmail.com>
2014-06-29 15:31:46 -0700

    Fixing a redirect

--- frips/js/main.js
+++ frips/js/main.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
     	$("#fb-login-btn").click(function () {
     		FB.login(function(response){
     			if (response.status === 'connected') {
-    				$(location).attr('href','/');
+    				$(location).attr('href','/frips');
     	    	    return false;
     			} else {
     				console.log('Not connected to Facebook');
