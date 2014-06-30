c02a7c2efffa1cdf201b3eea2441bb56d893cae8
Emre Colak <colemre@gmail.com>
2014-06-29 17:35:23 -0700

    Playing with graph api

--- frips/js/main.js
+++ frips/js/main.js
@@ -63,6 +63,13 @@ $(document).ready(function() {
 	        var $ul = $(this);
 	        $.each(cities, function (i, city) {
 	        	$ul.append($("<li>").append($("<a>").attr("href", "#").append(city).click(function () {
+	        		FB.api('/search?type=place&q=' + city, function (response) {
+	        			if (!response.errors) {
+	        				console.log(errors);
+	        			} else {
+	        				console.log(response.errors);
+	        			}
+	        		});
 	        		console.log("Going to " + city);
 	        	})));
 	        });
